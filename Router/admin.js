4566d86f80d9b5069f255baabcb205e8f00da350
dlwnsgmI <junjason1126@gmail.com>
2023-02-10 03:26:19 -0800

    login

--- Router/admin.js
+++ Router/admin.js
@@ -4,11 +4,22 @@ const connection = require('../model/db');
 const info = require("../config/info.json")
 
 Router.get('/', (req, res) => {
-  res.render('login')
+  if(req.session.loggedin){
+    res.render("admin")
+  } else {
+    res.render("login")
+  }
+  console.log(req.session.loggedin)
 })
 
-Router.get('/authentication', (req, res) => {
-  res
+Router.post('/authentication', (req, res) => {
+  console.log(req.body.name, req.body.password, info.users.Admin)
+  user = req.body.name;
+  password = req.body.password;
+  if(user == info.users.Admin.name && password == info.users.Admin.password){
+    req.session.loggedin = true
+  }
+  res.redirect('/admin')
 })
 
 module.exports = Router
