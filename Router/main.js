1e8108e909a78744a739c792392a3948559abb11
dlwnsgmI <junjason1126@gmail.com>
2023-02-13 22:47:32 -0800

    async err fixed

--- Router/main.js
+++ Router/main.js
@@ -4,31 +4,33 @@ let Router = express.Router();
 const info = require("../config/info.json");
 const { Configuration, OpenAIApi } = require("openai");
 const connection = require("../model/db");
-
+const async = require("async");
 
 connection.query("SELECT * FROM scala", (err, rows) => {
   console.log(rows[0].title);
 });
-
-Router.get('/', (req, res) => {
-  connection.query("SHOW TABLES", (err, rows) => {
-    var article = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
-    (async() => {
-      for(let i = 1; typeof rows[i] != 'undefined'; i ++){
-        /* console.log(rows[i].Tables_in_automated_blog) */
-        connection.query(`SELECT * FROM ??`, [rows[i].Tables_in_automated_blog], (err, rows) => {
-          console.log('aa')
+var article = [];
+connection.query("SHOW TABLES", (err, rows) => {
+  for(let i = 0; typeof rows[i] != 'undefined'; i++){
+    async.waterfall([
+      function(callback){
+        connection.query(`SELECT * FROM ??`, [rows[i].Tables_in_automated_blog] ,(err, rows) => {
           for(let j = 0; typeof rows[j] != 'undefined'; j++){
-            /* console.log(rows[j].title); */
+            if(j == 0){
+              article[i] = []
+            }
             article[i][j] = rows[j];
           }
+          callback(null, article)
         })
-      };
-      
-      await res.render('index', {article: article});
-      console.log(article)
-    })();
-  })
+      }, 
+    ])
+  }
+})
+
+Router.get('/', (req, res) => {
+  console.log(article[1][0].text)
+  res.render('index', {article:article});
 })
 
 
