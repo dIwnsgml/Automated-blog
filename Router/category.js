97a20aee6abe63ef25f26f87b4a9489ee3d2703b
dlwnsgmI <junjason1126@gmail.com>
2023-03-07 21:45:05 -0800

    rm all category btn

--- Router/category.js
+++ Router/category.js
@@ -1,6 +1,14 @@
 const express = require("express");
 let Router = express.Router();
 
+
+Router.get('/all', (req, res) => {
+  const category = 'all'
+  res.render('all-category', {category: category})
+  console.log(req.params.category)
+})
+
+
 Router.get('/:category', (req, res) => {
   const category = req.params.category
   res.render('category', {category: category})
